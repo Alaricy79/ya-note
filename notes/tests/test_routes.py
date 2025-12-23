3903ca025ff5c5b53b7af38b33188179d6c48275
Roman V. Yudanov <Alaricy@gmail.com>
2025-12-23 14:24:31 +0200

    home page

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.notes = Note.objects.create(title='Заголовок',
+                                        text='Текст',
+                                        author=cls.author)
+
+    def test_home_page(self):
+        url = reverse('notes:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
